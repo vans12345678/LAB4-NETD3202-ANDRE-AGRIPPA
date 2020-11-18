9563c278f317f7380743910cedf5a82e140ec285
home <andreagrippa76@gmail.com>
2020-11-17 21:15:44 -0500

    Added book appraisal functionality

--- LAB4-NETD3202-ANDRE-AGRIPPA/Controllers/HomeController.cs
+++ LAB4-NETD3202-ANDRE-AGRIPPA/Controllers/HomeController.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * Name: Andre Agrippa
+ * File: HomeController.cs
+ * Date: 11/17/2020
+ * Purpose: HomeController that calls views of various pages
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,15 +15,33 @@ namespace LAB4_NETD3202_ANDRE_AGRIPPA.Controllers
 {
     public class HomeController : Controller
     {
-        // public string Index()
-        // {
-        //     FirstModel message = new FirstModel();
-        //     return View(message);
-        // }
+        List<BookModel> bookList = new List<BookModel>();
         public IActionResult Index()
         {
-            FirstModel message = new FirstModel();
+            BookModel message = new BookModel();
             return View(message);
+        }
+
+        [HttpGet]
+        public IActionResult Appraise()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Appraise(BookModel book)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewData["Message"] = "Your textbook: " + book.title + ",  Version: " + book.version + 
+                                      "Was appraised at: $" + book.CalculateAppraise(book.price, book.condition);
+                return View("YourAppraisal", book);
+            }
+            else
+            {
+                 return View("Fail");
+            }
+           
         }
     }
 }
---
+++ LAB4-NETD3202-ANDRE-AGRIPPA/Models/BookModel.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Name: Andre Agrippa
+ * File: BookModel.cs
+ * Date: 11/17/2020
+ * Purpose: BookModel class that has attributes of a book
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB4_NETD3202_ANDRE_AGRIPPA.Models
+{
+    public class BookModel
+    {
+        //Getters and setters for textbook
+        public string title { get; set; }
+
+        public Int64 isbn { get; set; }
+
+        public double version { get; set; }
+
+        public double price { get; set; }
+
+        public string condition { get; set; }
+
+        //Default constructor 
+        public BookModel()
+        {
+
+        }
+        //Parameterized constructor
+        public BookModel(string title, int isbn, double version, double price, string condition)
+        {
+            this.title = title;
+            this.isbn = isbn;
+            this.version = version;
+            this.price = price;
+            this.condition = condition;
+        }
+        public double CalculateAppraise(double price, string condition)
+        {
+            //Formula to calculate appraisal price
+            if (condition == "Bad")
+            {
+                return (price / 4);
+            }
+            if (condition == "Good")
+            {
+                return (price / 3);
+            }
+            else
+            {
+                return (price / 2);
+            }
+        }
+    }
+}
